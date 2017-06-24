70e67a1872d12b41356bc7034209c37aa3914bed
Alex Cruz <mazdarider23@gmail.com>
2017-06-24 16:13:35 +0200

    ZenHub: Add SELinux switch
    Squashed commits:
    
    commit ce05f7dc7ad333b1234fee001e54ebd3e695634e
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Thu Dec 22 18:05:55 2016 +0100
    
        Selinux Switch
    
        Change-Id: Idd38f844f94bdc81e2b690682c73e468156ea10b
    
    commit 6b29cf14f8c25f8ee476d86225da3faa7c632a21
    Author: LorDClockaN <lordclockan@gmail.com>
    Date:   Thu Dec 22 18:11:05 2016 +0100
    
        Selinux Switch: Show toast only for permissive on boot
    
        Change-Id: I4e0589e09904332cf0dead5fe2bc9f416caf812e
    
    commit 217411c19ac0c0ac0bfd47b15d00f3d33406fa57
    Author: Meninblack007 <sanyam.53jain@gmail.com>
    Date:   Thu Dec 22 18:57:39 2016 +0100
    
        Settings: Fix selinux switch (#601)
    
        * Cleanup whitespaces
        * Use SharedPreference to save values
    
        Change-Id: I8dd13c3d2f6a16e4633933b5d750c9db412aa8e5
        Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>
    
    Author: SpiritCroc <spiritcroc@gmail.com>
    
        Use our SuShell utils, handle su denied, use boolean in
        shared pref, enhance our root extras fragment for
        initialization, ...
    
    Change-Id: I2469c45a794f960102534efc97f2e0c26e812987
    
    commit 75b568bfb11c70f6d386d7d27cc34aff9d550237
    Author: SpiritCroc <spiritcroc@gmail.com>
    
    @idoybh (DerpFest) edits: Adapt, organize, improve some code
    
    Co-authored-by: SpiritCroc <spiritcroc@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- src/com/zenx/zen/hub/fragments/Misc.java
+++ src/com/zenx/zen/hub/fragments/Misc.java
@@ -15,13 +15,20 @@
  */
 package com.zenx.zen.hub.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.app.Activity;
+import android.os.SELinux;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import androidx.preference.*;
 import android.provider.Settings;
+import android.util.Log;
+
+import com.zenx.zen.hub.utils.SuShell;
+import com.zenx.zen.hub.utils.SuTask;
 
 import com.android.internal.logging.nano.MetricsProto; 
 import com.android.settings.SettingsPreferenceFragment;
@@ -36,8 +43,13 @@ public class Misc extends SettingsPreferenceFragment
     public static final String TAG = "Misc";
 
     private static final String GAMING_MODE_ENABLED = "gaming_mode_enabled";
+    private static final String SELINUX_CATEGORY = "selinux";
+    private static final String PREF_SELINUX_MODE = "selinux_mode";
+    private static final String PREF_SELINUX_PERSISTENCE = "selinux_persistence";
 
     private SystemSettingMasterSwitchPreference mGamingMode;
+    private SwitchPreference mSelinuxMode;
+    private SwitchPreference mSelinuxPersistence;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,6 +61,19 @@ public class Misc extends SettingsPreferenceFragment
         mGamingMode.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.GAMING_MODE_ENABLED, 0) == 1));
         mGamingMode.setOnPreferenceChangeListener(this);
+
+        // SELinux
+        Preference selinuxCategory = findPreference(SELINUX_CATEGORY);
+        mSelinuxMode = (SwitchPreference) findPreference(PREF_SELINUX_MODE);
+        mSelinuxMode.setChecked(SELinux.isSELinuxEnforced());
+        mSelinuxMode.setOnPreferenceChangeListener(this);
+
+        mSelinuxPersistence =
+            (SwitchPreference) findPreference(PREF_SELINUX_PERSISTENCE);
+        mSelinuxPersistence.setOnPreferenceChangeListener(this);
+        mSelinuxPersistence.setChecked(getContext()
+            .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE)
+            .contains(PREF_SELINUX_MODE));
     }
 
     @Override
@@ -58,8 +83,55 @@ public class Misc extends SettingsPreferenceFragment
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.GAMING_MODE_ENABLED, value ? 1 : 0);
             return true;
-        }
+       } else if (preference == mSelinuxMode) {
+            boolean enabled = (Boolean) newValue;
+            new SwitchSelinuxTask(getActivity()).execute(enabled);
+            setSelinuxEnabled(enabled, mSelinuxPersistence.isChecked());
+            return true;
+      } else if (preference == mSelinuxPersistence) {
+            setSelinuxEnabled(mSelinuxMode.isChecked(), (Boolean) newValue);
+            return true;
+      }
         return false;
+    }
+
+        private void setSelinuxEnabled(boolean status, boolean persistent) {
+      SharedPreferences.Editor editor = getContext()
+          .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE).edit();
+      if (persistent) {
+        editor.putBoolean(PREF_SELINUX_MODE, status);
+      } else {
+        editor.remove(PREF_SELINUX_MODE);
+      }
+      editor.apply();
+      mSelinuxMode.setChecked(status);
+    }
+
+    private class SwitchSelinuxTask extends SuTask<Boolean> {
+      public SwitchSelinuxTask(Context context) {
+        super(context);
+      }
+      @Override
+      protected void sudoInBackground(Boolean... params) throws SuShell.SuDeniedException {
+        if (params.length != 1) {
+          Log.e(TAG, "SwitchSelinuxTask: invalid params count");
+          return;
+        }
+        if (params[0]) {
+          SuShell.runWithSuCheck("setenforce 1");
+        } else {
+          SuShell.runWithSuCheck("setenforce 0");
+        }
+      }
+
+      @Override
+      protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+          // Did not work, so restore actual value
+          setSelinuxEnabled(SELinux.isSELinuxEnforced(), mSelinuxPersistence.isChecked());
+        }
+      }
     }
 
     @Override
